544e4908d103e548677d77b7d5f53e5ba055db4e
Otavio Santana Ramos <otavio.omak@gmail.com>
2021-04-19 12:25:09 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/ramos/course/entities/User.java
+++ src/main/java/com/ramos/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.ramos.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
@@ -20,6 +21,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
