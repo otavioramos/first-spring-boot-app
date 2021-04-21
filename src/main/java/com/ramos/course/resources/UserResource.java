72fed9aabfacd77957d34c31530a3cc6269dae12
Otavio Ramos <otavio.omak@gmail.com>
2021-04-21 19:34:42 -0300

    User insert

--- src/main/java/com/ramos/course/resources/UserResource.java
+++ src/main/java/com/ramos/course/resources/UserResource.java
@@ -1,14 +1,14 @@
+
 package com.ramos.course.resources;
 
 import com.ramos.course.entities.User;
 import com.ramos.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,4 +30,14 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
 }
