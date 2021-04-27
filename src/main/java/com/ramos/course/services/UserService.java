e8386c72b66cf1bf42e073e1caaa588be32495f2
Otavio Santana Ramos <otavio.omak@gmail.com>
2021-04-27 14:25:25 -0300

    Exception handling - update

--- src/main/java/com/ramos/course/services/UserService.java
+++ src/main/java/com/ramos/course/services/UserService.java
@@ -1,17 +1,17 @@
 package com.ramos.course.services;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.ramos.course.entities.User;
+import com.ramos.course.repositories.UserRepository;
+import com.ramos.course.services.exceptions.DatabaseException;
+import com.ramos.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import com.ramos.course.entities.User;
-import com.ramos.course.repositories.UserRepository;
-import com.ramos.course.services.exceptions.DatabaseException;
-import com.ramos.course.services.exceptions.ResourceNotFoundException;
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -43,9 +43,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getOne(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getOne(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
